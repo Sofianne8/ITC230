40b6c72c3faea03f33f2fac4f870b9b8a2f7818e
Sofianne Battikh <sofianne8@gmail.Com>
2017-04-18 00:03:05 -0700

    Add files via upload

---
+++ music.js
@@ -0,0 +1,33 @@
+/**
+ * Created by sofiannebattikh on 4/14/17.
+ */
+'use strict'
+let songs = [
+  {title: "Volcano", author: "Damien Rice", pubDate: 2002},
+  {title: "Like A Stone", author: "AudioSlave", pubDate: 2002},
+  {title: "Let's get it on", author: "Marvin Gaye", pubDate: 1973}
+];
+
+
+exports.delete = (title) => {
+  let oldLength = songs.length;
+  let newSongs = songs.filter((item) => {
+    return item.title !== title;
+  });
+  songs = newSongs;
+  return {
+    deleted: songs.length !== oldLength,
+    total: songs.length,
+    Song: songs
+  }
+};
+console.log(this.item);
+console.log(this.delete("Volcano"));
+
+exports.get = (title) => {
+  return songs.find((item) => {
+    return item.title === title;
+  });
+};
+
+
